f7f20ddb1d86d804a447e50383190f7b3275e643
Josh <timmer.jo@gmail.com>
2015-06-09 17:27:51 -0600

    Date is now show properly at the top

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@ static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer  *s_date_layer;
 static GFont s_time_font;
+static GFont s_date_font;
 static BitmapLayer  *s_background_layer;
 static GBitmap  *s_background_bitmap;
 
@@ -17,7 +18,7 @@ static void update_time()  {
   static char time_buffer[] = "00:00";
   
   //Create a buffer for the date
-  static char date_buffer[] = "jan 01";
+  static char date_buffer[] = "jan01";
   
   //Insert the current hour and minutes into the buffer
   if(clock_is_24h_style()==true)  {
@@ -27,10 +28,11 @@ static void update_time()  {
     //Use 12 hour format
     strftime(time_buffer, sizeof("00:00"), "%I:%M", tick_time);
   }
-  strftime(date_buffer, sizeof"jan 01", "%h %d", tick_time);
+  strftime(date_buffer, sizeof"jan01", "%h%d", tick_time);
   
   //Put this time into the text layer
   text_layer_set_text(s_time_layer, time_buffer);
+  text_layer_set_text(s_date_layer, date_buffer);
 }
 
 static void main_window_load(Window *window)  {
@@ -41,7 +43,7 @@ static void main_window_load(Window *window)  {
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
   
   
-  
+  //TIME LAYER
   //Creates the text layer setting the background colour, text colour,
   //and starting text
   s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
@@ -53,18 +55,36 @@ static void main_window_load(Window *window)  {
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
   
   
-  //Add some style to the watchface
+  //Add some style to the watchface time
   //Apply font to text layer
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   
   //Sets the text layer as a child to the base window layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  
+  //DATE LAYER
+  s_date_layer = text_layer_create(GRect(5, 0, 139, 50));
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorWhite);
+  text_layer_set_text(s_date_layer, "jan 01");
+  
+  
+  //Add some style to the watchface date
+  //Apply font to text layer
+  text_layer_set_font(s_date_layer, s_time_font);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentLeft);
+  
+  //Sets the text layer as a child to the base window layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
 }
 
 static void main_window_unload(Window *window)  {
-  //Removes the text layer when closing
+  //Removes the time text layer when closing
   text_layer_destroy(s_time_layer);
+  //Removes the date text layer when closing
+  text_layer_destroy(s_date_layer);
+  //Removes the font for the date and time
   fonts_unload_custom_font(s_time_font);
   //destroys GBitmap (image)
   gbitmap_destroy(s_background_bitmap);
